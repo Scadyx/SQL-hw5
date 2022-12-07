d4c591acff4f2aa20177152fc124d8cd11e7b99a
Scadyx <scadyx0@gmail.com>
2022-12-07 16:13:36 +0200

    add validation and fixed api host

--- hw2/main.py
+++ hw2/main.py
@@ -4,27 +4,32 @@ from fastapi import FastAPI, Body
 import uvicorn
 
 
-connection = psycopg2.connect(user="postgres",
-                                password="postgres",
-                                host="postgres",
-                                port="5432",
-                                database="postgres")
+connection = psycopg2.connect(
+    user="postgres",
+    password="postgres",
+    host="postgres",
+    port="5432",
+    database="postgres"
+)
 
 connection.autocommit = True
 cursor = connection.cursor()
 
 def init_db(file_path: str):
-    sql_users = '''CREATE TABLE IF NOT EXISTS Users (id int,
-    name varchar(255),
-    last_name varchar(255),
-    time_created int,
-    gender varchar(255),
-    age varchar(255),
-    city varchar(255),
-    birth_day varchar(255),
-    premium BOOL,
-    ip varchar(255),
-    balance float4);'''
+    sql_users = '''CREATE TABLE IF NOT EXISTS Users (
+                   id SERIAL PRIMARY KEY,
+                   name varchar(255),
+                   last_name varchar(255),
+                   time_created int,
+                   gender varchar(255),
+                   age varchar(255),
+                   city varchar(255),
+                   birth_day varchar(255),
+                   premium BOOL,
+                   ip varchar(255),
+                   balance float4)
+                   ;'''
+
     sql_insert = 'INSERT INTO users ({}) VALUES ({})'
     cursor.execute(sql_users)
     with open(file_path, 'r') as f:
@@ -34,23 +39,23 @@ def init_db(file_path: str):
             res_keys = str(list(n_user.keys())).strip("[]").replace("'", '')
             res_val = str(list(n_user.values())).strip("[]")
             cursor.execute(sql_insert.format(res_keys, res_val))
-        print ('Inserted')
+        print('Inserted')
 
 def create_tables():
     sql_createBets = '''CREATE TABLE IF NOT EXISTS Bets (
-    id int,
+    id SERIAL PRIMARY KEY,
     date_created int,
-    userId int,
-    eventId int );'''
+    userId int REFERENCES users(id),
+    eventId int REFERENCES events(id) );'''
     sql_createEvents = '''CREATE TABLE IF NOT EXISTS Events (
-    id int,
+    id SERIAL PRIMARY KEY,
     type varchar(255),
     name varchar(255),
     event_date varchar(255) );'''
-    cursor.execute(sql_createBets)
-    print("Table Bets Created")
     cursor.execute(sql_createEvents)
     print("Table Events Created")
+    cursor.execute(sql_createBets)
+    print("Table Bets Created")
 
 
 file = 'data.jsonl'
@@ -63,6 +68,7 @@ def get_users():
     query = "SELECT * FROM users"
     cursor.execute(query)
     return cursor.fetchall()
+
 
 @app.get("/user")
 def get_users(id):
@@ -84,17 +90,20 @@ def update_user(id,val):
     cursor.execute(query)
     return "updated {} in user with id {}".format(val, id)
 
+
 @app.post("/user")
 def create_user(col, val):
     query = "INSERT INTO users ({}) VALUES  ({}) ".format(col, val)
     cursor.execute(query)
     return "created user {}".format(val)
 
+
 @app.get("/bets")
 def get_bets():
     query = "SELECT * FROM bets"
     cursor.execute(query)
     return cursor.fetchall()
+
 
 @app.get("/bet")
 def get_bet(id):
@@ -105,7 +114,7 @@ def get_bet(id):
 
 @app.delete("/bet")
 def delete_bet(id):
-    query = "DELETE FROM bet WHERE id = {}".format(id)
+    query = "DELETE FROM bets WHERE id = {}".format(id)
     cursor.execute(query)
     return "Deleted bet with id {}".format(id)
 
@@ -117,7 +126,6 @@ def update_bet(id, val):
     return "updated {} in bet with id {}".format(val, id)
 
 
-
 @app.post("/bet")
 def create_bet(col, val):
 
@@ -125,11 +133,13 @@ def create_bet(col, val):
     cursor.execute(query)
     return "created bet {}".format(val)
 
+
 @app.get("/events")
 def get_events():
     query = "SELECT * FROM events"
     cursor.execute(query)
     return cursor.fetchall()
+
 
 @app.get("/event")
 def get_events(id):
@@ -152,7 +162,6 @@ def update_events(id, val):
     return "updated {} in bet with id {}".format(val, id)
 
 
-
 @app.post("/event")
 def create_events(col, val):
 
@@ -160,5 +169,6 @@ def create_events(col, val):
     cursor.execute(query)
     return "created event {}".format(val)
 
+
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- hw3/main.py
+++ hw3/main.py
@@ -76,4 +76,4 @@ async def run_unique_names_histogram():
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
